4359e2368a13fce8492ca708711a42f8585ad572
Ananda Afryan Gemilang <anandalark007@gmail.com>
2022-08-01 00:40:37 +0700

    added navbar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 import IndexPage from './views';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<IndexPage/>}></Route>
-    </Routes>
+    <div>
+      <Navbar/>
+      <div className='grid grid-cols-1  md:grid-cols-3'>
+        <div>
+          <Sidebar/>
+        </div>
+        <div className='bg-gray-300 md:col-span-2'>
+          <Routes>
+            <Route path='/' element={<IndexPage/>}></Route>
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/CardPersonnel.jsx
+++ src/components/CardPersonnel.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
-export default function CardPersonnel() {
+export default function CardPersonnel(props) {
+  const {
+    firstName,
+    lastName,
+    phoneNumber,
+    birthdayDate,
+    emailAddress,
+    personnelId
+  } = props
+
+  const checkPersonnelId = () => {
+    if (personnelId && personnelId.includes('undefined') || !personnelId) {
+      return '-';
+    }
+    return personnelId;
+  }
+
   return (
     <div>
       <div className='bg-white p-3 text-blue-900'>
-        <div className="upper-section flex justify-between">
+        <div className="upper-section flex justify-between border-b border-gray-300 pb-3">
           <p className='text-blue-900'>
-            Personnel ID: <span className='text-pink-600'>123456</span>
+            Personnel ID: <span className='text-pink-600'>{checkPersonnelId()}</span>
           </p>
           <div>
             <button>
@@ -15,24 +31,28 @@ export default function CardPersonnel() {
           </div>
         </div>
         <div className="photo-section">
-
+          <div className=' w-24 h-24 rounded-full border border-blue-900 mx-auto my-10'>
+            <span className='m-auto flex justify-center items-center h-full'>Photo</span>
+          </div>
         </div>
         <div>
           <div className='mt-2'>
             <p className='font-semibold'>Name</p>
-            <p className='text-xl'>First Name Last Name</p>
+            <p className='text-xl'>{firstName} {lastName}</p>
           </div>
           <div className='mt-2'>
             <p className='font-semibold'>Telephone</p>
-            <p className='text-xl'>Phone Number</p>
+            <p className='text-xl'>{phoneNumber}</p>
           </div>
-          <div className='mt-2'>
-            <p className='font-semibold'>Birthday</p>
-            <p className='text-xl'>DD-MM-YYYY</p>
-          </div>
-          <div className='mt-2'>
-            <p className='font-semibold'>Email</p>
-            <p className='text-xl'>Email Address</p>
+          <div className='hidden md:block'>
+            <div className='mt-2'>
+              <p className='font-semibold'>Birthday</p>
+              <p className='text-xl'>{birthdayDate}</p>
+            </div>
+            <div className='mt-2'>
+              <p className='font-semibold'>Email</p>
+              <p className='text-xl break-all'>{emailAddress}</p>
+            </div>
           </div>
         </div>
       </div>
